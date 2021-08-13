5da681321cff398205838a2d8e88259705e26830
Rodrigo Torres <rtorres@intermediait.com>
2021-08-13 12:41:33 -0300

    Initial commit

---
+++ Intermarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intermarket
+//
+//  Created by Rodrigo Torres on 13/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
