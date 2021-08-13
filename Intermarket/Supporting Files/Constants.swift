3f09c96723666f45d7d5d8ca2788bce799d2e963
Rodrigo Torres <rtorres@intermediait.com>
2021-08-13 16:10:21 -0300

    Add CuserState + Constants

---
+++ Intermarket/Supporting Files/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Intermarket
+//
+//  Created by Rodrigo Torres on 13/08/2021.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    
+    struct SplashScreen {
+        static let Primary = UIColor(named: "SplashScreenPrimary")
+    }
+    
+}
