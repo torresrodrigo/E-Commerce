77947a21eadf78050bbddcd333477d8c56f9d424
Rodrigo Torres <rtorres@intermediait.com>
2021-09-15 10:46:19 -0300

    Added MoreViewController UI

--- Intermarket/ViewControllers/Tabs/More/MoreViewController.swift
+++ Intermarket/ViewControllers/Tabs/More/MoreViewController.swift
@@ -13,17 +13,31 @@ import FacebookLogin
 
 class MoreViewController: UIViewController {
     
+    @IBOutlet weak var profileButton: UIButton!
+    @IBOutlet weak var logOutButton: UIButton!
     let userDefaults = UserDefaults.standard
     let firebaseAuth = Auth.auth()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
     }
     @IBAction func actionLogOut(_ sender: Any) {
         removeUserDefaults()
         GIDSignIn.sharedInstance.signOut()
         LoginManager().logOut()
         firebaseLogOut()
+    }
+    
+    func setupUI() {
+        setupButtons()
+    }
+    
+    func setupButtons() {
+        profileButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 21, bottom: 0, right: 0)
+        profileButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 19, bottom: 0, right: 0)
+        logOutButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 21, bottom: 0, right: 0)
+        logOutButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 19, bottom: 0, right: 0)
     }
     
     func firebaseLogOut() {
