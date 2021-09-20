1ce3c770ad99923a3047aea3635e3526dcd9d4be
Rodrigo Torres <rtorres@intermediait.com>
2021-09-20 13:13:43 -0300

    Refactor DetailViewController

---
+++ Intermarket/ViewControllers/Tabs/Search/Cells/FeatureTableViewCell/FeaturesTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  FeaturesTableViewCell.swift
+//  Intermarket
+//
+//  Created by Rodrigo Torres on 17/09/2021.
+//
+
+import UIKit
+
+class FeaturesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var featureLabel: UILabel!
+    
+    static let identifier = String(describing: FeaturesTableViewCell.self)
+    static func nib() -> UINib {
+        return UINib(nibName: String(describing: FeaturesTableViewCell.self), bundle: nil)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func setupCell(forFeatureName featureName: String, forFeatureValue featureValue: String?) {
+        guard let value = featureValue else {
+            featureLabel.text = "\(featureName): Not description"
+            return }
+        featureLabel.text = "\(featureName): \(value)"
+    }
+    
+}
