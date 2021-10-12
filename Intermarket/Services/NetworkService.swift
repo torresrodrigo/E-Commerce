8913ae907435e73f2acadeed5f4b66c9fc3823d1
Rodrigo Torres <rtorres@intermediait.com>
2021-10-12 17:36:59 -0300

    Changes in NetworkService also SearchViewController and DetailViewController callbacks.

--- Intermarket/Services/NetworkService.swift
+++ Intermarket/Services/NetworkService.swift
@@ -13,44 +13,58 @@ class NetworkService {
     
     static let shared = NetworkService()
     
-    func getProducts(query: String, completed: @escaping (Result<BaseProducts,Error>) -> Void) {
+    func getProducts(query: String, success: @escaping (_ products: [Products]) -> (), failure: @escaping (_ error: Error?) -> ()) {
         let finalUrl = String(describing: Endpoints.Search+query)
-        AF.request(finalUrl).responseData { response in
-            switch response.result {
-            case .success(let data):
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                do {
-                    let result = try decoder.decode(BaseProducts.self, from: data)
-                    completed(.success(result))
-                } catch {
-                    completed(.failure(error))
-                }
-            case .failure(let error):
-                print(error)
-                print(response.response?.statusCode ?? 0)
-                completed(.failure(error))
+        AF.request(finalUrl).responseDecodable(of: BaseProducts.self) { response in
+//            switch response.result {
+//            case .success(let data):
+//                let decoder = JSONDecoder()
+//                decoder.keyDecodingStrategy = .convertFromSnakeCase
+//                do {
+//                    let result = try decoder.decode(BaseProducts.self, from: data)
+//                    completed(.success(result))
+//                } catch {
+//                    completed(.failure(error))
+//                }
+//            case .failure(let error):
+//                print(error)
+//                print(response.response?.statusCode ?? 0)
+//                completed(.failure(error))
+//            }
+            if let products = response.value?.results {
+                success(products)
+            }
+            else {
+                failure(response.error)
             }
         }
     }
     
-    func getDetailsProducts(query: String, completed: @escaping (Result<DetailProduct,Error>) -> Void) {
+    func getDetailsProducts(query: String, sucess: @escaping (_ detail: DetailProduct) -> (), failure: @escaping (_ error: Error?) -> ()) {
         let finalUrl = String(describing: Endpoints.DetailsProducts+query)
-        AF.request(finalUrl, method: .get).responseData { response in
-            switch response.result {
-            case .success(let data):
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .useDefaultKeys
-                do {
-                    let result = try decoder.decode(DetailProduct.self, from: data)
-                    completed(.success(result))
-                } catch {
-                    completed(.failure(error))
-                }
-            case .failure(let error):
-                print(error)
-                print(response.response?.statusCode ?? 200)
+        AF.request(finalUrl, method: .get).responseDecodable(of: DetailProduct.self) { response in
+//            switch response.result {
+//            case .success(let data):
+//                let decoder = JSONDecoder()
+//                decoder.keyDecodingStrategy = .useDefaultKeys
+//                do {
+//                    let result = try decoder.decode(DetailProduct.self, from: data)
+//                    completed(.success(result))
+//                } catch {
+//                    completed(.failure(error))
+//                }
+//            case .failure(let error):
+//                print(error)
+//                print(response.response?.statusCode ?? 200)
+//            }
+            
+            if let detail = response.value {
+                sucess(detail)
             }
+            else {
+                failure(response.error)
+            }
+            
         }
     }
     
