3e5bc1f05502f45dd6927950fae5dd90b9b5d00d
Rodrigo Torres <rtorres@intermediait.com>
2021-10-12 23:17:57 -0300

    Revert "Refactor in NetworkService.swift"
    This reverts commit c6a5cf48673e1156db68ce03fcfa104b3850c929.

--- Intermarket/Services/NetworkService.swift
+++ Intermarket/Services/NetworkService.swift
@@ -16,6 +16,21 @@ class NetworkService {
     func getProducts(query: String, success: @escaping (_ products: [Products]) -> (), failure: @escaping (_ error: Error?) -> ()) {
         let finalUrl = String(describing: Endpoints.Search+query)
         AF.request(finalUrl).responseDecodable(of: BaseProducts.self) { response in
+//            switch response.result {
+//            case .success(let data):
+//                let decoder = JSONDecoder()
+//                decoder.keyDecodingStrategy = .convertFromSnakeCase
+//                do {
+//                    let result = try decoder.decode(BaseProducts.self, from: data)
+//                    completed(.success(result))
+//                } catch {
+//                    completed(.failure(error))
+//                }
+//            case .failure(let error):
+//                print(error)
+//                print(response.response?.statusCode ?? 0)
+//                completed(.failure(error))
+//            }
             if let products = response.value?.results {
                 success(products)
             }
@@ -28,12 +43,28 @@ class NetworkService {
     func getDetailsProducts(query: String, sucess: @escaping (_ detail: DetailProduct) -> (), failure: @escaping (_ error: Error?) -> ()) {
         let finalUrl = String(describing: Endpoints.DetailsProducts+query)
         AF.request(finalUrl, method: .get).responseDecodable(of: DetailProduct.self) { response in
+//            switch response.result {
+//            case .success(let data):
+//                let decoder = JSONDecoder()
+//                decoder.keyDecodingStrategy = .useDefaultKeys
+//                do {
+//                    let result = try decoder.decode(DetailProduct.self, from: data)
+//                    completed(.success(result))
+//                } catch {
+//                    completed(.failure(error))
+//                }
+//            case .failure(let error):
+//                print(error)
+//                print(response.response?.statusCode ?? 200)
+//            }
+            
             if let detail = response.value {
                 sucess(detail)
             }
             else {
                 failure(response.error)
             }
+            
         }
     }
     
