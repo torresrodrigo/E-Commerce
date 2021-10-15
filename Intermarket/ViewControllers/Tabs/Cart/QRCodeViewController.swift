c6655a0fb53299949429ac3f4356ddfc2cd531ef
Rodrigo Torres <rtorres@intermediait.com>
2021-10-15 14:15:52 -0300

    Fix bug in QRCodeViewController

--- Intermarket/ViewControllers/Tabs/Cart/QRCodeViewController.swift
+++ Intermarket/ViewControllers/Tabs/Cart/QRCodeViewController.swift
@@ -15,13 +15,17 @@ class QRCodeViewController: UIViewController {
 
     @IBOutlet weak var qrView: UIView!
     @IBOutlet weak var qrLabel: UILabel!
-    var video = AVCaptureVideoPreviewLayer()
-    let session = AVCaptureSession()
+    var videoPreviewLayer = AVCaptureVideoPreviewLayer()
+    let captureSession = AVCaptureSession()
     var isNavigationController = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestPermission()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        checkPermissionCamera()
     }
     
     @IBAction func backButton(_ sender: Any) {
@@ -29,7 +33,7 @@ class QRCodeViewController: UIViewController {
     }
     
     private func backButtonAction() {
-        if isNavigationController == false {
+        if !isNavigationController {
             self.navigationController?.popToRootViewController(animated: true)
         }
         else {
@@ -45,90 +49,169 @@ extension QRCodeViewController: AVCaptureMetadataOutputObjectsDelegate {
     //Initial configuation
     func setConfiguration() {
         guard let captureDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) else {
-            print("Camera device not founded")
+            print("Failed to get the camera device")
             return
         }
         
         do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            session.addInput(input)
+            //Set Input device on the capture session
+            let captureMetadataInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(captureMetadataInput)
+            
+            //Initialize a AVCaptureMetadataOutput object and set it as te output device to the capture session
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetadataOutput)
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+            // Initialize the vide preview layer and add it as a sublayer to the  viewPreview view's layer
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer.frame = qrView.layer.bounds
+            qrView.layer.addSublayer(videoPreviewLayer)
+            
+            // Start video capture
+            captureSession.startRunning()
+            
+            
         } catch {
             print("Error")
         }
         
-        let output = AVCaptureMetadataOutput()
-        session.addOutput(output)
-        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-        
-        video = AVCaptureVideoPreviewLayer(session: session)
-        video.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        video.frame = qrView.layer.bounds
-        qrView.layer.addSublayer(video)
-        session.startRunning()
     }
     
-    func requestPermission() {
-        let auth = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
-        switch(auth) {
-        case .denied, .notDetermined, .restricted:
-            showAlert()
-        case .authorized:
-            proceedWithCameraAccess()
-        }
-    }
-    
-    //Permision to camera access
-    func proceedWithCameraAccess(){
-        AVCaptureDevice.requestAccess(for: .video) { success in
-          if success {
-            DispatchQueue.main.async {
-                self.setConfiguration()
-            }
-          }
-        }
-      }
-    
-    func showAlert() {
-        let alert = UIAlertController(title: "Camera", message: "Camera access is absolutely necessary to use this app", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-        }))
-        self.present(alert, animated: true)
-    }
-    
-    //Executed camera
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         if metadataObjects.count == 0 {
             qrView.frame = CGRect.zero
-            qrLabel.text = "Escanéa el QR de un producto"
+            qrLabel.text = "Codigo QR no encontrado"
         }
+        
         
         let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
         
         if metadataObj.type == AVMetadataObject.ObjectType.qr {
-            guard let barCodeObject = video.transformedMetadataObject(for: metadataObj) else {return }
-            qrView.frame = barCodeObject.bounds
-            qrLabel.text = "Producto entrado"
-            
-            let alert = UIAlertController(title: "Producto encontrado", message: "¿Que deseas hacer?", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Agregar", style: .default, handler: { action in
-                self.goToCart()
-            }))
-            alert.addAction(UIAlertAction(title: "Cancelar", style: .default, handler: { action in
-                self.qrLabel.text = "Escanéa el QR de un producto"
-                self.session.startRunning()
-            }))
-            self.present(alert, animated: true, completion: nil)
-            
+            let barCodeObject = videoPreviewLayer.transformedMetadataObject(for: metadataObj)
+            qrView.frame = barCodeObject!.bounds
+            qrLabel.text = "Producto encontrado"
+            self.alertProductFounded()
+                        
             if metadataObj.stringValue != nil {
-                print("worked")
+                print("Worked")
             }
         }
         
-        self.session.stopRunning()
-        
+        self.captureSession.stopRunning()
     }
+    
+    func alertProductFounded() {
+        let alert = UIAlertController(title: "Producto encontrado", message: "¿Que deseas hacer?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Agregar", style: .default, handler: { action in
+            self.goToCart()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .default, handler: { action in
+            self.qrLabel.text = "Escanéa el QR de un producto"
+            self.captureSession.startRunning()
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+//    func requestPermission() {
+//        let auth = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+//        switch(auth) {
+//        case .denied, .notDetermined, .restricted:
+//            showAlert()
+//        case .authorized:
+//            proceedWithCameraAccess()
+//        }
+//    }
+//
+//    //Permision to camera access
+//    func proceedWithCameraAccess(){
+//        AVCaptureDevice.requestAccess(for: .video) { success in
+//          if success {
+//            DispatchQueue.main.async {
+//                self.setConfiguration()
+//            }
+//          }
+//        }
+//      }
+    
+    
+    //Check Permission Camera
+    func checkPermissionCamera() {
+        let authorizationStatus = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+        switch authorizationStatus {
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: AVMediaType.video) { [weak self] (granted) in
+                if granted {
+                    print("Access Granted")
+                    DispatchQueue.main.async { [weak self] in
+                        self?.setConfiguration()
+                    }
+                }
+                else {
+                    print("Access Denied")
+                    DispatchQueue.main.async { [weak self] in
+                        self?.showAlertPermissionCamera()
+                    }
+                }
+            }
+        case .authorized:
+            print("Access authorized")
+            setConfiguration()
+        case .denied, .restricted:
+            print("Restricted")
+            showAlertPermissionCamera()
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func showAlertPermissionCamera() {
+        let alert = UIAlertController(title: "Camera", message: "Intermarket necesita acceso a la camara para escanear el codigo QR", preferredStyle: .alert)
+        let settingsAction = UIAlertAction(title: "Configuración", style: .default) { (action) in
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else { return }
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl) { (_) in
+                }
+            }
+        }
+        alert.addAction(settingsAction)
+        present(alert, animated: true)
+    }
+    
+//    //Executed camera
+//    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+//        if metadataObjects.count == 0 {
+//            qrView.frame = CGRect.zero
+//            qrLabel.text = "Escanéa el QR de un producto"
+//        }
+//
+//        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+//
+//        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+//            guard let barCodeObject = video.transformedMetadataObject(for: metadataObj) else {return }
+//            qrView.frame = barCodeObject.bounds
+//            qrLabel.text = "Producto entrado"
+//
+//            let alert = UIAlertController(title: "Producto encontrado", message: "¿Que deseas hacer?", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "Agregar", style: .default, handler: { action in
+//                self.goToCart()
+//            }))
+//            alert.addAction(UIAlertAction(title: "Cancelar", style: .default, handler: { action in
+//                self.qrLabel.text = "Escanéa el QR de un producto"
+//                self.session.startRunning()
+//            }))
+//            self.present(alert, animated: true, completion: nil)
+//
+//            if metadataObj.stringValue != nil {
+//                print("worked")
+//            }
+//        }
+//
+//        self.session.stopRunning()
+//
+//    }
     
     private func goToCart() {
         let storyboard = UIStoryboard(name: Identifier.TabBarStoryboard, bundle: nil)
